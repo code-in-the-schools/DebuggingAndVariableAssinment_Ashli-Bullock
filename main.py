35b448784e150d10ca40180f1c6aa55fafec2856
AshliB <ashlibullock16@gmail.com>
2020-07-15 15:21:06 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+example = "example"
+print("This is an", example, "and has no errors.")
+
+name = "Elia Deppe"
+fav_artist = "Earl Sweatshirt"
+vacation = "Nosara"
+hobby = "Painting, Skateboarding"
+today= "7/15/20"
+
+print("My name is, Elia Deppe , and my favorite artist is, Earl Sweatshirt")
+print("I love to go on vacation to", vacation, "and my two main hobbies are", hobby)
+print("Today's date is: 7/15/20")
